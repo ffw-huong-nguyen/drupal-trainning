2c820ffd2905ec634e79ed05c260c72ea3ae926f
ffw-huong-nguyen <huong.nguyen@ffwagency.com>
2022-07-14 18:53:34 +0700

    Update Container, Header, Footer, Banner

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,31 @@
-// (function (Drupal, drupalSettings, once) {
-//   Drupal.behaviors.myBehavior = {
-//     attach: function (context, settings) {
-//       once('myBehavior', 'html', context).forEach( function () {
-//         myFunction();
-//       })
-//     }
-//   }
-// } (Drupal, drupalSettings, once));
+(function (Drupal, drupalSettings, once) {
+  // Drupal.behaviors.myBehavior = {
+  //   attach: function (context, settings) {
+  //     once('myBehavior', 'html', context).forEach( function () {
+  //       myFunction();
+  //     })
+  //   }
+  // }
 
 (function ($) {
   $(document).ready(function ($) {
     $(document).on('click', '.js-open-menu', function () {
       $('.header-main__menu').toggle();
       $('body').addClass('show-menu');
+      });
+    });
+
+    $('.js-banner-slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 2000,
+      autoplay: true,
+      arrows: true,
+      dots: true,
+      prevArrow: '<span class="slick-arrow left icon-chevron-down" aria-label="Previous" type="span"></span>',
+      nextArrow: '<span class="slick-arrow right icon-chevron-down" aria-label="Next" type="span"></span>'
     });
   });
+
 })(jQuery);
